3ddee10c586a5375b66c04987bdfeb1275f936e4
luan.tran <peacehihi@gmail.com>
2018-04-02 22:48:27 +0700

    edit dum mas

--- Dump20180402.sql
+++ Dump20180402.sql
@@ -19,7 +19,7 @@
 -- Table structure for table `calendar`
 --
 
-DROP TABLE IF EXISTS `calendar`;
+//DROP TABLE IF EXISTS `calendar`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `calendar` (
